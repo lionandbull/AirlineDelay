a08e548a8f555dc1fa72377c9940c0c629a4f19c
Wade <liuweixi0819@gmail.com>
2019-06-02 20:49:31 -0400

    update title

---
+++ precache-manifest.a6eb5bcf123b5df2a114fe3952063dab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f6b9b3427b10cf76fbda44f49503fb9",
+    "url": "/AirlineDelay/index.html"
+  },
+  {
+    "revision": "a57ad0327e1948f13cf8",
+    "url": "/AirlineDelay/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "f2500f02779df82fd22b",
+    "url": "/AirlineDelay/static/css/main.dd60fd72.chunk.css"
+  },
+  {
+    "revision": "a57ad0327e1948f13cf8",
+    "url": "/AirlineDelay/static/js/2.c1757f48.chunk.js"
+  },
+  {
+    "revision": "f2500f02779df82fd22b",
+    "url": "/AirlineDelay/static/js/main.4a6b0a1d.chunk.js"
+  },
+  {
+    "revision": "401557ab02a3ed5988d7",
+    "url": "/AirlineDelay/static/js/runtime~main.ea9fe5c8.js"
+  }
+]);
